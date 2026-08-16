cb658f54dc31bb251c78a731def5d09460e6635e
Carlos Tey <cteyr123@gmail.com>
2026-08-16 19:03:11 +0200

    Creando cabecera y el menu desplegable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 
 import './App.scss'
+import { useState } from 'react';
+
+
+
+import logoHeader from './design/images/logo.svg'
 
 function App() {
+  const [abierto, setAbierto] = useState(false);
+
 
   return (
-    <div className="main-container"></div>
-      
+    <div className="mainContainer">
+      <div className="headerContainer">
+        <img src={logoHeader} alt="" />
+
+        <div className='dropdownMenu'>
+          <button className='menu' onClick={() => setAbierto(!abierto)}>Units ↓</button>
+
+          {abierto && (
+            <div>
+              <p>Temperature</p>
+
+              <button>Celsius (°C) ✓</button>
+              <button>Fahrenheit (°F)</button>
+
+              <p>Wind Speed</p>
+
+              <button>km/h ✓</button>
+              <button>mph</button>
+
+              <p>Precipitation</p>
+
+              <button>Milimeters</button>
+            </div>
+          )}
+        </div>
+
+      </div>
+
+    </div>
+
   )
 }
 
