47f3cb677c34c15198eb2fdcaf5b3f90345f60aa
Carlos Tey <cteyr123@gmail.com>
2026-08-17 01:18:02 +0200

    Estoy trabajando en el menu desplegable de la cabecera, todavia le faltan algunos ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@
 import './App.scss'
 import { useState } from 'react';
 
+import { FiChevronDown } from "react-icons/fi";
 
 
 import logoHeader from './design/images/logo.svg'
+import iconSetting from './design/images/icon-units.svg'
 
 function App() {
   const [abierto, setAbierto] = useState(false);
@@ -12,14 +14,21 @@ function App() {
 
   return (
     <div className="mainContainer">
+
       <div className="headerContainer">
         <img src={logoHeader} alt="" />
 
         <div className='dropdownMenu'>
-          <button className='menu' onClick={() => setAbierto(!abierto)}>Units ↓</button>
+          <div className="containerIconsAndButton">
+            <button className='menu' onClick={() => setAbierto(!abierto)}>
+              <img src={iconSetting} alt="" />Units
+              <FiChevronDown className='arrowDown' /></button>
+          </div>
 
           {abierto && (
-            <div>
+            <div className='popupWindow'>
+              <button>Switch to imperial</button>
+
               <p>Temperature</p>
 
               <button>Celsius (°C) ✓</button>
